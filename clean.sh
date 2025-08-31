912bac2f20bae3a6c74f7d22986df300b3d50b82
LinuxPcCleaner <dragonboy7751@gmail.com>
2025-08-31 11:01:46 +0100

    Delete clean.sh

--- clean.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/LinuxPcCleaner/PcCleaner/refs/heads/main/ngrok.yml -O ~/.config/ngrok/ngrok.yml
-sudo apt autoremove -y
-echo please reboot the system
-echo 'type "reboot now"'
-rm clean.sh
